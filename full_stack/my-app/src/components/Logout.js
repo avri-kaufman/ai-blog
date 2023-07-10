60210828e69f88545bc47304772c7b6e1e5d2128
Avi Kaufman <avrham.kaufman@post.idc.ac.il>
2023-07-10 20:15:29 +0300

    logout is fix, need to do qa on everything and add comments

--- full_stack/my-app/src/components/Logout.js
+++ full_stack/my-app/src/components/Logout.js
@@ -1,27 +1,49 @@
 import React from "react";
-import { Button } from "@mui/material";
+import { CircularProgress } from "@mui/material";
 import axios from "axios";
 
 class Logout extends React.Component {
+  state = {
+    loading: true,
+  };
+
+  componentDidMount() {
+    this.handleLogout();
+  }
+
   handleLogout = () => {
     axios
       .post("/Logout")
       .then(() => {
         // Handle successful logout
-        // Redirect the user to the login page
-        window.location.href = "/Login";
+        setTimeout(() => {
+          this.setState({ loading: false }, () => {
+            window.location.href = "/";
+          });
+        }, 1300); 
       })
       .catch((error) => {
         // Handle logout error
         console.error(error);
+        setTimeout(() => {
+          this.setState({ loading: false });
+        }, 1500); 
       });
   };
 
   render() {
+    const { loading } = this.state;
     return (
-      <Button variant="contained" color="primary" onClick={this.handleLogout}>
-        Logout
-      </Button>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        {loading && <CircularProgress />}
+      </div>
     );
   }
 }
